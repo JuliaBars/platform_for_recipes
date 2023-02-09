7cfd14dbea3d2393dae8feddc94757a397a87d69
Юлия Орлова <zaikunets@yandex.ru>
2023-02-09 13:21:18 +0300

    CI #014 проблемы со статикой бека

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+1!$%&^&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
