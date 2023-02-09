7a448250590c9eeca45f21787d1c9b13bc121433
Юлия Орлова <zaikunets@yandex.ru>
2023-02-10 00:35:49 +0300

    Статика #021

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -145,7 +145,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static_backend'),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
