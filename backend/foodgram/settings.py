b070d847088d92489d7846a36ff193ad6f6f2c27
Юлия Орлова <zaikunets@yandex.ru>
2023-02-10 18:59:54 +0300

    CSRF #004

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,10 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+1!$%&^&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ["http://*"]
+CSRF_TRUSTED_ORIGINS = ['http://*']
 CSRF_TRUSTED_ORIGINS = ['http://158.160.24.150:83/']
+CSRF_COOKIE_DOMAIN = '158.160.24.150'
 
 
 # Application definition
