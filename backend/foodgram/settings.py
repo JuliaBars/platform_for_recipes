b0a81002b7216559d164d9fc77a62d7beeba004c
Юлия Орлова <zaikunets@yandex.ru>
2023-02-10 15:00:47 +0300

    CSRF fix and dump encoding

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+1!$%&^&*()_+')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://localhost']
 
 
 # Application definition
