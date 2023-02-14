4dde0d71ac3ff525c48c60dfeae1b9c8c671d44d
Юлия Орлова <zaikunets@yandex.ru>
2023-02-14 12:46:59 +0300

    fixing frontend footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -3,9 +3,9 @@ import { Container, LinkComponent } from '../index'
 
 const Footer = () => {
   return <footer className={styles.footer}>
-      <Container className={styles.footer__container}>
-        <LinkComponent href='#' title='Продуктовый помощник' className={styles.footer__brand} />
-      </Container>
+    <Container className={styles.footer__container}>
+      <LinkComponent href='/' title='Продуктовый помощник' className={styles.footer__brand} />
+    </Container>
   </footer>
 }
 
