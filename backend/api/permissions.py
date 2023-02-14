693c2b2374a28e4f24b498316fa73e02c1e3d36c
Юлия Орлова <zaikunets@yandex.ru>
2023-02-14 14:31:07 +0300

    fixing users/id/ for anonym #003

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,3 +14,15 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
+
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in SAFE_METHODS
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,19 +6,20 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
+from recipes.models import (FavouriteRecipe, Ingredient, Recipe,
+                            RecipeIngredient, ShoppingCart, Tag)
 from rest_framework import filters, status
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from recipes.models import (FavouriteRecipe, Ingredient, Recipe,
-                            RecipeIngredient, ShoppingCart, Tag)
 from users.models import Subscription
 
 from .filters import IngredientFilter, RecipeFilter
 from .pagination import CustomPagination
-from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
+from .permissions import (IsAdminOrReadOnly, IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly)
 from .serializers import (CustomUserSerializer, IngredientSerializer,
                           RecipeBaseSerializer, RecipeReadSerializer,
                           RecipeWriteSerializer, SubscribeSerializer,
@@ -31,12 +32,7 @@ class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     pagination_class = CustomPagination
-    # permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    def get_permissions(self):
-        if self.action in ('subscribe', 'subscriptions'):
-            return [IsAuthenticated()]
-        return super().get_permissions()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     @action(
         detail=True,
